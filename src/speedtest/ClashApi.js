0406a00579dbae3645854bd5f8513f0692b79404
wally <wally94@163.com>
2022-05-02 22:13:47 +0800

    啥也不是

---
+++ src/speedtest/ClashApi.js
@@ -0,0 +1,35 @@
+
+const axios = require("axios");
+const request = axios.create({
+    timeout: 10*1000, // 请求超时时间
+    baseURL: `http://127.0.0.1:9090`,
+})
+
+//重载配置文件
+module.exports.setConfigs=async (path)=>{
+  return  request.put("/configs",{path})
+}
+//获取配置
+module.exports.getConfigs=async ()=>{
+ return  request.get("/configs")
+}
+//获取所有的节点
+module.exports.getProxies=async ()=>{
+    return  request.get("/proxies")
+}
+
+//获取单个节点延迟
+//name节点名称
+//timeout超时时间
+//url测试的网址
+module.exports.getDelay=async (name,timeout,url="http://www.gstatic.com/generate_204")=>{
+    return  request.get(`/proxies/${encodeURIComponent(name)}/delay?timeout=${timeout}&url=${encodeURIComponent(url)}`)
+}
+
+//设置当前选择的节点
+//groups 分组
+//name 节点名
+module.exports.setSelector=async (groups,name)=>{
+    return  request.put(`/proxies/${encodeURIComponent(groups)}`,{name})
+}
+
---
+++ src/speedtest/index.js
@@ -0,0 +1,52 @@
+const yaml = require("yaml")
+const fs = require("fs")
+const {join}=require("path")
+const async=require("async")
+const clashApi=require("./ClashApi")
+const {proxies}=yaml.parse(fs.readFileSync("./nodes.yaml","utf8"))
+ //异步队列
+ const queue = async.queue((fn, completed) => {
+    fn().then(() => {
+        completed()
+    })
+}, 200)
+
+function speedtest(proxie){
+    return async ()=>{
+        try {
+            const res= await clashApi.getDelay(proxie.name,200)
+            nodes.proxies.push(proxie)
+            nodes["proxy-groups"][0].proxies.push(proxie.name)
+            console.log(`节点${proxie.name} 延时${res.data.delay}`);
+         } catch (error) {
+             console.log(`节点${proxie.name}网络错误或超时，剔除此节点`);
+         }
+    }
+}
+const nodes= {
+    proxies: [],
+    "proxy-groups": [{
+       name: "所有节点",
+       type: "select",
+       proxies: []
+   }]
+}
+//休眠函数
+function sleep(timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, timeout);
+    })
+}
+async function start() {
+    await clashApi.setConfigs(join(process.cwd(),"./nodes.yaml"))
+    await sleep(2000)
+    proxies.forEach(proxie => {
+        queue.push(speedtest(proxie))
+    });
+    await queue.drain()
+    console.log(nodes.proxies.length);
+    fs.writeFileSync("./speedtest.yaml",yaml.stringify(nodes))
+}
+start()
